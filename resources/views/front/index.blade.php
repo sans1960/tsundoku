6fb28ee1cc51510f9ea43e78b58d65f325f39087
sans1960 <asanscliment@gmail.com>
2024-04-11 16:33:36 +0200

    frontend3

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -51,7 +51,7 @@ TSUNDOKU
                 @foreach ($topratedbook as $book)
                 <div class="d-flex flex-column">
                     <a class="nav-link" href="{{route('book',$book)}}">
-                        <div class="card"
+                        <div class="card border-0"
                             style="height: 300px;background-image:url(@if ($book->imatge != null){{ $book->imatge }} @else {{Storage::url($book->foto)}} @endif);background-size:cover;background-position:center;object-fit:fill;"
                             data-bs-toggle="tooltip" data-bs-title="{{$book->titol}}">
 
@@ -83,7 +83,7 @@ TSUNDOKU
                 @if ($book->comentbook_count > 0)
                 <div class="d-flex flex-column">
                     <a class="nav-link" href="{{route('book',$book)}}">
-                        <div class="card"
+                        <div class="card border-0"
                             style="height: 300px;background-image:url(@if ($book->imatge != null){{ $book->imatge }} @else {{Storage::url($book->foto)}} @endif);background-size:cover;background-position:center;"
                             data-bs-toggle="tooltip" data-bs-title="{{$book->titol}}">
 
@@ -111,10 +111,12 @@ TSUNDOKU
                 @foreach ($novetats as $book)
                 <a href="{{route('book',$book)}}" class="nav-link" data-bs-toggle="tooltip"
                     data-bs-title="{{$book->titol}}">
-                    <div class="card"
+                    <div class="card border-0 d-flex justify-content-center align-items-center"
                         style="height: 300px;background-image:url(@if ($book->imatge != null){{ $book->imatge }} @else {{Storage::url($book->foto)}} @endif);background-size:cover;background-position:center;">
 
-
+                        @if ($book->auto == 1)
+                        <p class="bg-success text-white p-1 rounded">Autopublicat</p>
+                        @endif
                     </div>
 
                 </a>
@@ -142,10 +144,12 @@ TSUNDOKU
                 @foreach ($estrenes as $book)
                 <a href="{{route('book',$book)}}" class="nav-link" data-bs-toggle="tooltip"
                     data-bs-title="{{$book->titol}}">
-                    <div class="card"
+                    <div class="card border-0 d-flex justify-content-center align-items-center"
                         style="height: 300px;background-image:url(@if ($book->imatge != null){{ $book->imatge }} @else {{Storage::url($book->foto)}} @endif);background-size:cover;background-position:center;object-fit:fill;">
 
-
+                        @if ($book->auto == 1)
+                        <p class="bg-success text-white p-1 rounded">Autopublicat</p>
+                        @endif
                     </div>
 
                 </a>
@@ -163,7 +167,7 @@ TSUNDOKU
 
                 <a href="{{route('autor',$autor)}}" class="nav-link" data-bs-toggle="tooltip"
                     data-bs-title="{{$autor->autor_nom}}">
-                    <div class="card d-flex justify-content-center align-items-center"
+                    <div class="card d-flex justify-content-center align-items-center border-0"
                         style="height: 300px;background-image:url( @if ($autor->url_foto != null){{ $autor->url_foto }} @else {{Storage::url($autor->image)}} @endif);background-size:cover;background-position:center;object-fit:fill;">
 
                     </div>
@@ -183,9 +187,9 @@ TSUNDOKU
 
                 <a href="{{route('post',$post)}}" class="nav-link" data-bs-toggle="tooltip"
                     data-bs-title="{{$post->titol}}">
-                    <div class="card d-flex justify-content-center align-items-center"
-                        style="height: 300px;background-image:url( {{ Storage::url($post->image) }} );background-size:cover;background-position:center;object-fit:fill;">
-
+                    <div class="card d-flex justify-content-end align-items-center border-0 p-3 "
+                        style="height: 300px;background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url( {{ Storage::url($post->image) }} );background-size:cover;background-position:center;object-fit:fill;">
+                        <h5 class="text-uppercase text-white">{{ $post->titol }}</h5>
                     </div>
 
                 </a>
@@ -200,7 +204,7 @@ TSUNDOKU
                     @foreach ($books as $book)
                     <a href="{{route('book',$book)}}" class="nav-link" data-bs-toggle="tooltip"
                         data-bs-title="{{$book->titol}}">
-                        <div class="card d-flex justify-content-center align-items-center"
+                        <div class="card d-flex justify-content-center align-items-center border-0"
                             style="height: 300px;background-image:url(@if ($book->imatge != null){{ $book->imatge }} @else {{Storage::url($book->foto)}} @endif);background-size:cover;background-position:center;object-fit:fill;">
                             @if ($book->auto == 1)
                             <p class="bg-success text-white p-1 rounded">Autopublicat</p>
@@ -238,9 +242,9 @@ TSUNDOKU
 
                     <a href="{{route('medi',$medi)}}" class="nav-link" data-bs-toggle="tooltip"
                         data-bs-title="{{$medi->titol}}">
-                        <div class="card d-flex justify-content-center align-items-center"
-                            style="height: 300px;background-image:url( {{ Storage::url($medi->image) }} );background-size:cover;background-position:center;object-fit:fill;">
-
+                        <div class="card d-flex justify-content-end align-items-center p-3"
+                            style="height: 300px;background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url( {{ Storage::url($medi->image) }} );background-size:cover;background-position:center;object-fit:fill;">
+                            <h5 class="text-uppercase text-white">{{ $medi->titol }}</h5>
                         </div>
 
                     </a>
@@ -264,9 +268,9 @@ TSUNDOKU
                 @foreach ($actes as $acte)
                 <a href="{{route('acte',$acte)}}" class="nav-link" data-bs-toggle="tooltip"
                     data-bs-title="{{$acte->titol}}">
-                    <div class="card d-flex justify-content-center align-items-center"
-                        style="height: 300px;background-image:url( {{ Storage::url($acte->image) }} );background-size:cover;background-position:center;object-fit:fill;">
-
+                    <div class="card d-flex justify-content-end align-items-center p-3"
+                        style="height: 300px;background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url( {{ Storage::url($acte->image) }} );background-size:cover;background-position:center;object-fit:fill;">
+                        <h5 class="text-uppercase text-white">{{ $acte->titol }}</h5>
                     </div>
                     <p>{{\Carbon\Carbon::parse($acte->data)->format('d/m/Y')}}</p>
                 </a>
